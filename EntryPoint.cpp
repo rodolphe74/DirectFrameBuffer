a45356d737a71fe4c3c81353f30691c88c461a5c
rodolphe74 <rcordenie@gmail.com>
2024-04-25 08:22:14 +0200

    Draw function pointer

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -3,12 +3,31 @@
 #include <windows.h>
 #include <d3d9.h>
 
-void draw(char* data, int w, int h, int pitch, bool tictac) {
+void drawInterferences(char* data, int w, int h, int pitch, bool tictac) {
 	for (int y = 0; y < h; ++y) {
 		DWORD* row = (DWORD*)data;
 		for (int x = 0; x < w; ++x) {
 			int r = 0 + std::rand() / ((RAND_MAX + 1u) / 255);
 			*row++ = D3DCOLOR_XRGB(255 - r * y / h, 255 * x / w, 255 * y / h);
+		}
+		data += pitch;
+	}
+}
+
+void drawMoire(char* data, int w, int h, int pitch, bool tictac) {
+	for (int y = 0; y < h; ++y) {
+		DWORD* row = (DWORD*)data;
+		for (int x = 0; x < w; ++x) {
+			if (y % 3)
+				*row++ = tictac ? D3DCOLOR_XRGB(95, 74, 124) : D3DCOLOR_XRGB(0, 255, 0);
+			else{
+				*row++ = tictac ? D3DCOLOR_XRGB(0, 255, 0) : D3DCOLOR_XRGB(95, 74, 124);
+			}
+			if (x % 3)
+				*row++ = tictac ? D3DCOLOR_XRGB(95, 74, 124) : D3DCOLOR_XRGB(0, 255, 0);
+			else {
+				*row++ = tictac ? D3DCOLOR_XRGB(0, 255, 0) : D3DCOLOR_XRGB(95, 74, 124);
+			}
 		}
 		data += pitch;
 	}
@@ -22,7 +41,7 @@ int WINAPI WinMain(HINSTANCE _In_ hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	Application a;
 	a.waitRR(false);
 	a.createWindow(hInstance, "Minimal DX window", 640, 480);
-	a.messagesLoop(draw);
+	a.messagesLoop(drawInterferences);
 	Chronometer::meanLap("Mean");
 	return 0;
 }
